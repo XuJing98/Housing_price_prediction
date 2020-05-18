bd8fc881fee88a6130cfde7c4f0d03368ea2281d
Xu Jing <13309618@student.uts.edu.au>
2020-05-18 14:50:46 +1000

    prediction with ptype attribute

--- prediction.py
+++ prediction.py
@@ -2,7 +2,7 @@ import joblib
 import numpy as np
 
 
-def pred(Beds, Baths, Cars, Area, Date, Lat, Long, Dis, NSW):
+def pred(Beds, Baths, Cars, Area, Date, Lat, Long, Dis, Ptype, NSW):
 
     """
     This is the function for predicting the housing price
@@ -14,6 +14,8 @@ def pred(Beds, Baths, Cars, Area, Date, Lat, Long, Dis, NSW):
     Lat : Latitude (float)
     Long : Longitude (float)
     Dis : the Distance to the nearest train station (km) (float)
+    Ptype(str):  the type of the house ('House', 'House: One Storey / Lowset', 'House: Two Storey / Highset'
+    'House: Semi Detached', 'Unit','Unit: Standard', 'Unit: Townhouse/Villa')
     NSW : the postcode of the house (int)
     we use the NSW to select the model, and now the suburb we have trained are
     NSW 2112: Ryde, NSW 2114: West Ryde, NSW 2118: Carlingford, NSW 2119: Beecroft, NSW 2120: Thornleigh, Pennant Hills,
@@ -21,12 +23,25 @@ def pred(Beds, Baths, Cars, Area, Date, Lat, Long, Dis, NSW):
     NSW 2154: Castle Hill.
     """
 
-    filename = './models/NSW' + str(NSW) + '.sav'
+    filename = './models/' + str(NSW) + '.sav'
     # load the model from disk
     loaded_model = joblib.load(filename)
-    data = np.array([Beds, Baths, Cars, Area, Date, Lat, Long, Dis]).reshape(1,-1)
+    if Ptype == 'House':
+        data = np.array([Beds, Baths, Cars, Area, Date, Lat, Long, Dis, 1, 0, 0, 0, 0, 0, 0]).reshape(1, -1)
+    elif Ptype == 'House: One Storey / Lowset':
+        data = np.array([Beds, Baths, Cars, Area, Date, Lat, Long, Dis, 0, 1, 0, 0, 0, 0, 0]).reshape(1, -1)
+    elif Ptype == 'House: Semi Detached':
+        data = np.array([Beds, Baths, Cars, Area, Date, Lat, Long, Dis, 0, 0, 1, 0, 0, 0, 0]).reshape(1, -1)
+    elif Ptype == 'House: Two Storey / Highset':
+        data = np.array([Beds, Baths, Cars, Area, Date, Lat, Long, Dis, 0, 0, 0, 1, 0, 0, 0]).reshape(1, -1)
+    elif Ptype == 'Unit':
+        data = np.array([Beds, Baths, Cars, Area, Date, Lat, Long, Dis, 0, 0, 0, 0, 1, 0, 0]).reshape(1, -1)
+    elif Ptype == 'Unit: Standard':
+        data = np.array([Beds, Baths, Cars, Area, Date, Lat, Long, Dis, 0, 1, 0, 0, 0, 0, 0]).reshape(1, -1)
+    else:
+        data = np.array([Beds, Baths, Cars, Area, Date, Lat, Long, Dis, 0, 1, 0, 0, 0, 0, 0]).reshape(1, -1)
     prediction = loaded_model.predict(data)
     return round(prediction.item(), 2)
 
 
-# print(pred(Beds=2, Baths=1, Cars=1, Area=200, Date=20, Lat=-33, Long=151, Dis=1.5, NSW=2154))
+# print(pred(Beds=2, Baths=1, Cars=1, Area=200, Date=20, Lat=-33, Long=151, Dis=1.5, NSW=2112, Ptype='House'))
